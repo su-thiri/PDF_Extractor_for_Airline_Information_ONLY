efd1bffc8dda16936dcc7578c21c9942723dac96
su-thiri <thirishwehlaing9@gmail.com>
2024-01-22 17:15:59 +0630

    feat : update url

--- update_news/urls.py
+++ update_news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import upload_image, generated,upload_list,agent_generate
 
 urlpatterns = [
-    path('upload/', upload_image, name='upload_image'),
+    path('', upload_image, name='upload_image'),
     path('generated/<int:pk>/', generated, name='generated'),
     path('upload_list/',upload_list,name="upload_list"),
     path('agent_generate/<int:pk>/',agent_generate,name="agent_generate")
